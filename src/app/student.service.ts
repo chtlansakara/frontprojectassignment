e5fed0e84da49b922229a1d0ceb3214d47445acc
voidcht <chtlansakara@gmail.com>
2025-12-22 14:00:30 +0530

    Student service class methods added.

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -17,4 +17,23 @@ export class StudentService {
     return this.httpClient.get<Student[]>(this.link);
   }
 
+  //method to get student by id
+  getStudentsById(id:Number):Observable<Student>{
+    return this.httpClient.get<Student>(this.link+id);
+  }
+
+  //method to create a new student
+  addStudent(student: Student):Observable<Student>{
+    return this.httpClient.post<Student>(this.link,student);
+  }
+
+  //method to update a student
+  updateStudent(id:Number, student:Student):Observable<Student>{
+    return this.httpClient.put<Student>(this.link+id, student);
+  }
+
+  //method to delete a student
+  deleteStudent(id:Number):Observable<String>{
+    return this.httpClient.delete<String>(this.link+id);
+  }
 }
