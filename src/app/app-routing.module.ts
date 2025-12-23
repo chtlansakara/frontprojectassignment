b85d12bac8c4f67a479d871c5d408342ffeaaade
voidcht <chtlansakara@gmail.com>
2025-12-24 00:43:08 +0530

    Student's project add,delete functionality added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { StudentListComponent } from './student-list/student-list.component';
 import { StudentFormComponent } from './student-form/student-form.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { AssignmentComponent } from './assignment/assignment.component';
+import { StudentProjectsComponent } from './student-projects/student-projects.component';
 
 const routes: Routes = [
   {path:"students", component:StudentListComponent},
   {path:"students/addStudent", component:StudentFormComponent},
   {path:"students/updateStudent/:id", component: StudentFormComponent},
+  {path:"students/:id/projects", component: StudentProjectsComponent},
   {path:"projects", component:ProjectListComponent},
   {path:"assignment", component:AssignmentComponent},
   {path:"",redirectTo:"students", pathMatch:"full"}
--- src/app/student-projects/student-projects.component.ts
+++ src/app/student-projects/student-projects.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
+import { Project } from '../project';
+import { StudentService } from '../student.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-student-projects',
@@ -6,6 +10,74 @@ import { Component } from '@angular/core';
   templateUrl: './student-projects.component.html',
   styleUrl: './student-projects.component.css'
 })
-export class StudentProjectsComponent {
+export class StudentProjectsComponent implements OnInit {
+  // empty Student object
+  student: Student = new Student("",0,[]);
+  // empty Project array - for available projects
+  availableProjectsList : Project[] = [];
+  // variable to hold the project id - being added
+  projectId: any;
 
+  // injecting services
+  constructor(
+    private studentService :StudentService,
+    private activatedoute : ActivatedRoute
+  ){}
+
+  
+  ngOnInit(): void {
+      //to extract the relevant student id from the activated route 
+      this.student.id = this.activatedoute.snapshot.params["id"];
+      //update the class properties with relevant student data
+      this.refreshPage();
+  }
+
+  //find the student by the id and load to the Student object
+  getStudentById(){
+    this.studentService.getStudentsById(Number(this.student.id)).subscribe(
+      data => {
+        //assign the returned data to the Student object
+        this.student = data;
+      }
+    )
+  }
+
+  //find available projects for the student
+  getAvailableProjects(){
+    this.studentService.getAvailableStudentProjects(this.student.id).subscribe(
+      data =>{
+        //assigning to the class variable
+        this.availableProjectsList = data;
+      }
+    )
+  }
+
+  //method to refresh the page -to call both above functions
+  refreshPage(){
+    //load to student object
+    this.getStudentById();
+    //load to available project list
+    this.getAvailableProjects();
+  }
+
+  //method to add a new project to student's projects list
+  addProject(){
+    this.studentService.addProjectToStudent(this.student.id, this.projectId).subscribe(
+      data => {
+        //refresh page to load the component properties again from backend
+        this.refreshPage();
+      }
+    )
+  }
+
+  //method to delete a new project to student's projects list
+  deleteProject(projectId: any){
+    //we use the parameter for projectId here
+    this.studentService.deleteProjectOfStudent(this.student.id, projectId).subscribe(
+      data => {
+        //refresh page to load the component properties again from backend
+        this.refreshPage();
+      }
+    )
+  }
 }
